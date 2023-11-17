1add671a0087cfabd494a1bc985e32794d0c5c7a
jingwei <a0979580915@gmail.com>
2023-11-17 23:28:44 +0800

    fix path

--- data.py
+++ data.py
@@ -5,14 +5,14 @@ import os
 from datetime import datetime
 
 
-with open('html.2023.final.data/sno_test_set.txt') as file:
+with open('/Users/xujinwei/Desktop/ML/html.2023.final.data/sno_test_set.txt') as file:
     sno_test = [line.strip() for line in file]
 
-All_date = os.listdir("html.2023.final.data/release/")
+All_date = os.listdir("/Users/xujinwei/Desktop/ML/html.2023.final.data/release/")
 
 df = pd.DataFrame(columns=['SNO', 'Date', 'Time', 'totalminutes', 'dayofweek', 'lat', 'lng', 'sbi'])
 
-with open("html.2023.final.data/demographic.json", 'r') as f:
+with open("/Users/xujinwei/Desktop/ML/html.2023.final.data/demographic.json", 'r') as f:
     demographic_data = json.load(f)
 
 for date in All_date:
@@ -21,7 +21,7 @@ for date in All_date:
         lat = item.get("lat")
         lng = item.get("lng")
 
-        json_file = "html.2023.final.data/release/" + date + "/" + sno + ".json"
+        json_file = "/Users/xujinwei/Desktop/ML/html.2023.final.data/release/" + date + "/" + sno + ".json"
 
         if os.path.exists(json_file):
             with open(json_file, 'r') as file:
