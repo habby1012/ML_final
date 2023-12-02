eb3bf37350908df86b288f0db19cc08a94155117
jingwei <a0979580915@gmail.com>
2023-12-02 23:44:58 +0800

    totaltime -> sin_time and cos_time

--- data.py
+++ data.py
@@ -1,22 +1,40 @@
 import json
 import pandas as pd
-import json
 import os
+import random
+import numpy as np
 from datetime import datetime, timedelta
 
+# get all stations
+with open('/Users/xujinwei/Desktop/ML/html.2023.final.data/demographic.json', 'r') as f:
+    demographic_data = json.load(f)
+all_stations = set(demographic_data.keys())
+
+# get test stations
 with open('/Users/xujinwei/Desktop/ML/html.2023.final.data/sno_test_set.txt') as file:
-    sno_test = [line.strip() for line in file]
+    sno_test = set([line.strip() for line in file])
+
+# randomly choose stations except those test stations
+available_stations = all_stations - sno_test
+random_stations = random.sample(list(available_stations), 100)
+
+# stations taken to train model
+selected_stations = list(sno_test)
+# selected_stations = list(sno_test) + random_stations
+# selected_stations = list(all_stations)
 
 All_date = os.listdir("/Users/xujinwei/Desktop/ML/html.2023.final.data/release/")
 
-df = pd.DataFrame(columns=['SNO', 'Date', 'Time', 'totalminutes', 'dayofweek', 'lat', 'lng', 'sbi'])
+output_file = "data.csv"
+df = pd.DataFrame(columns=['SNO', 'Date', 'Time', 'sin_time', 'cos_time', 'dayofweek', 'lat', 'lng', 'sbi'])
+df.to_csv(output_file, mode='w', index=False)
 
 with open("/Users/xujinwei/Desktop/ML/html.2023.final.data/demographic.json", 'r') as f:
     demographic_data = json.load(f)
 
 miss = 0
 for date in All_date:
-    for sno in sno_test:
+    for sno in selected_stations:
         item = demographic_data.get(str(sno), {})
         lat = item.get("lat")
         lng = item.get("lng")
@@ -41,36 +59,38 @@ for date in All_date:
                                 hours = time_obj.hour
                                 minutes = time_obj.minute
                                 total_minutes = hours * 60 + minutes
+                                sin_time = np.sin(2 * np.pi * total_minutes / 1440)
+                                cos_time = np.cos(2 * np.pi * total_minutes / 1440)
 
                                 date_str = str(date)
                                 datetime_str = f"{date_str[:4]}-{date_str[4:6]}-{date_str[6:]} {time}"
                                 datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M")
                                 day_of_week = datetime_obj.weekday()
                                 
-                                new_rows.append({'SNO': sno, 'Date': date, 'Time': formatted_time, 'totalminutes': total_minutes, 'dayofweek': day_of_week, 'lat': lat, 'lng': lng, 'sbi': sbi})
+                                new_rows.append({'SNO': sno, 'Date': date, 'Time': formatted_time, 'sin_time': sin_time, 'cos_time': cos_time, 'dayofweek': day_of_week, 'lat': lat, 'lng': lng, 'sbi': sbi})
 
                             miss = 0
 
                         else:
+                            tot = values['tot']
                             sbi = values['sbi']
                             
                             time_obj = datetime.strptime(time, "%H:%M")
                             hours = time_obj.hour
                             minutes = time_obj.minute
                             total_minutes = hours * 60 + minutes
+                            sin_time = np.sin(2 * np.pi * total_minutes / 1440)
+                            cos_time = np.cos(2 * np.pi * total_minutes / 1440)
 
                             date_str = str(date)
                             datetime_str = f"{date_str[:4]}-{date_str[4:6]}-{date_str[6:]} {time}"
                             datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M")
                             day_of_week = datetime_obj.weekday()
 
-                            new_rows.append({'SNO': sno, 'Date': date, 'Time': time, 'totalminutes': total_minutes, 'dayofweek': day_of_week, 'lat': lat, 'lng': lng, 'sbi': sbi})
+                            new_rows.append({'SNO': sno, 'Date': date, 'Time': time, 'sin_time': sin_time, 'cos_time': cos_time, 'dayofweek': day_of_week, 'lat': lat, 'lng': lng, 'sbi': sbi})
 
                     else:
                         miss += 1
 
                 new_df = pd.DataFrame(new_rows)
-                df = pd.concat([df, new_df], ignore_index=True)
-
-output_file_path = 'data.csv'
-df.to_csv(output_file_path, index=False)
+                new_df.to_csv(output_file, mode='a', index=False, header=False)
--- train_partial.py
+++ train_partial.py
@@ -4,12 +4,11 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 import datetime
 import json
-import category_encoders as ce
 
 df = pd.read_csv('data.csv')
 
-target_encoder = ce.TargetEncoder()
-df['SNO'] = target_encoder.fit_transform(df['SNO'], df['sbi'])
+df['SNO'] = df['SNO'].astype(str)
+df = pd.get_dummies(df, columns=['SNO'])
 
 X = df.drop(['Date', 'Time', 'sbi'], axis=1)
 y = df['sbi']
@@ -19,7 +18,7 @@ X.to_csv('see_train.csv', mode='a', index=False, header=False)
 
 # train model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-model = RandomForestRegressor(n_estimators=15)
+model = RandomForestRegressor(n_estimators=30)
 model.fit(X_train, y_train)
 
 print("Model trained.")
@@ -43,20 +42,24 @@ for prediction_date in prediction_dates:
                 cos_time = np.cos(2 * np.pi * total_minutes / 1440)
 
                 id_str = f"{prediction_date.strftime('%Y%m%d')}_{sno}_{hour:02d}:{minute:02d}"
-                sno_encoded = target_encoder.transform([[sno]]).iloc[0, 0]
                 row = {
                     'id': id_str,
-                    'totalminutes': total_minutes, 
+                    'sin_time': sin_time,
+                    'cos_time': cos_time, 
                     'dayofweek': prediction_date.weekday(),
                     'lat': demographic_data[sno]['lat'], 
                     'lng': demographic_data[sno]['lng'],
-                    'SNO': sno_encoded
+                    f'SNO_{sno}': 1
                 }
+                for sno_col in df.columns[df.columns.str.startswith('SNO_')]:
+                    if sno_col != f'SNO_{sno}':
+                        row[sno_col] = 0
+
                 prediction_data.append(row)
 
 prediction_df = pd.DataFrame(prediction_data)
 
-prediction_features = prediction_df[X_train.columns]
+prediction_features = prediction_df.drop('id', axis=1)
 predicted_sbi = model.predict(prediction_features)
 
 predicted_sbi_rounded = [round(value) for value in predicted_sbi]
